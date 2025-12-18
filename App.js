e7c7ef4feba8db5bc010503d1643664d9887e348
rommel dalisay <rommelmdalisay@gmail.com>
2025-12-18 13:50:03 +0800

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import ProductContainer from './Screeens/Product/ProductContainer';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello recat native</Text>
-      <StatusBar style="auto" />
+      <ProductContainer />
     </View>
   );
 }
---
+++ Screeens/Product/ProductContainer.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native'
+const data = require('../../assets/data/products.json')
+const ProductContainer = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        setProducts(data);
+
+        return () => {
+            setProducts([])
+        }
+    }, [])
+    return (
+        <View>
+            <Text>Product Container</Text>
+            <View style={{ marginTop: 200 }} >
+                <FlatList
+                    horizontal
+                    data={products}
+                    renderItem={({ item }) => <Text>{item.brand}</Text>}
+                    keyExtractor={item => item.name}
+                />
+            </View>
+        </View>
+    )
+}
+
+export default ProductContainer;
